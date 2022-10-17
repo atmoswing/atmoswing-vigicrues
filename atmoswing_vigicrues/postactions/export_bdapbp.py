15cb5257a59444e131ed066c7e6d82b16926f39a
Pascal Horton <pascal.horton@terranum.ch>
2022-10-17 23:20:31 +0200

    Starting to parse netcdf data.

--- atmoswing_vigicrues/postactions/export_bdapbp.py
+++ atmoswing_vigicrues/postactions/export_bdapbp.py
@@ -31,32 +31,60 @@ class ExportBdApBp(PostAction):
                 'date': self._get_metadata('date'),
                 'message': self._get_metadata('message')
             },
-            'metadata': {
-                'atmoswing': {
-                    'creation_date': None,
-                    'origin': None
-                },
-                'predictand': {
-                    'temporal_resolution': None,
-                    'dataset_id': None,
-                    'database': None,
-                    'station_ids': None
-                },
-                'description': {
-                    'method_id': None,
-                    'method_id_display': None,
-                    'specific_tag': None,
-                    'specific_tag_display': None,
-                },
-                'entites': {
-                    'names': None,
-                    'ids': None,
-                    'official_ids': None
-                },
-            },
-            'data': None,
-            'statistics': None
+            'metadata': self._create_metadata_block(),
+            'data': self._create_data_block(),
+            'statistics': self._create_statistics_block(),
         }
 
         with open(Path(self.output_dir) / self.file_name, "w") as outfile:
             json.dump(output, outfile)
+
+    def _create_metadata_block(self):
+        if not self._file_contents:
+            return None
+
+        nc_file = self._file_contents[0]
+
+        block = {
+            'atmoswing': {
+                'creation_date': nc_file.creation_date,
+                'origin': nc_file.origin
+            },
+            'predictand': {
+                'temporal_resolution': nc_file.predictand_temporal_resolution,
+                'dataset_id': nc_file.predictand_dataset_id,
+                'database': nc_file.predictand_database,
+                'station_ids': nc_file.predictand_station_ids
+            },
+            'description': {
+                'method_id': nc_file.method_id,
+                'method_id_display': nc_file.method_id_display,
+                'specific_tag': nc_file.specific_tag,
+                'specific_tag_display': nc_file.specific_tag_display,
+            },
+            'entites': {
+                'names': None,
+                'ids': None,
+                'official_ids': None
+            },
+        }
+
+        return block
+
+    def _create_data_block(self):
+        if not self._file_contents:
+            return None
+
+        block = {
+        }
+
+        return block
+
+    def _create_statistics_block(self):
+        if not self._file_contents:
+            return None
+
+        block = {
+        }
+
+        return block
