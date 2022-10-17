179b0cfa6082e3c0fe520dbdb8e64f5394def8ac
Pascal Horton <pascal.horton@terranum.ch>
2022-10-17 10:36:14 +0200

    Moving arguments checks to the main class.

--- atmoswing_vigicrues/__main__.py
+++ atmoswing_vigicrues/__main__.py
@@ -1,36 +1,5 @@
-import sys
 import argparse
-import yaml
-from atmoswing_vigicrues import atmoswing_vigicrues
-from pathlib import Path
-
-
-def check_arguments(args):
-
-    if not args.output_dir:
-        print(f'The output directory was not provided.')
-        sys.exit(1)
-
-
-def check_paths_exist(args):
-    batch_file = Path(args.batch_file)
-    if not batch_file.exists():
-        print(f'The file "{batch_file}" was not found.')
-        sys.exit(1)
-    if not batch_file.is_file():
-        print(f'The provided path "{batch_file}" is not a file.')
-        sys.exit(1)
-
-    path_output = Path(args.output_dir)
-    if not path_output.exists():
-        path_output.mkdir(parents=True, exist_ok=True)
-
-
-def load_config():
-    with open('config.yaml') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-
-    return config
+from atmoswing_vigicrues import AtmoSwingVigicrues
 
 
 def main() -> int:
@@ -43,12 +12,9 @@ def main() -> int:
 
     args = parser.parse_args()
 
-    check_arguments(args)
-    check_paths_exist(args)
+    atmoswing = AtmoSwingVigicrues(args)
 
-    config = load_config()
-
-    return atmoswing_vigicrues.run(Path(args.batch_file), config, args)
+    return atmoswing.run()
 
 
 if __name__ == "__main__":
--- atmoswing_vigicrues/atmoswing_vigicrues.py
+++ atmoswing_vigicrues/atmoswing_vigicrues.py
@@ -1,6 +1,42 @@
-def run(file_path, config, options) -> int:
+import yaml
+import sys
+from pathlib import Path
 
 
-    print('Calculation finished.')
+class AtmoSwingVigicrues:
+    """Class to manage AtmoSwing forecasts for the Vigicrues network."""
 
-    return 0
+    def __int__(self, options):
+        self.options = options
+        self.config = None
+        self._load_config()
+        self._check_options()
+        self._check_paths_exist()
+
+    def run(self, file_path, options) -> int:
+
+        print('Calculation finished.')
+
+        return 0
+
+    def _load_config(self):
+        with open('config.yaml') as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+
+    def _check_options(self):
+        if not self.options.output_dir:
+            print(f'The output directory was not provided.')
+            sys.exit(1)
+
+    def _check_paths_exist(self):
+        batch_file = Path(self.options.batch_file)
+        if not batch_file.exists():
+            print(f'The file "{batch_file}" was not found.')
+            sys.exit(1)
+        if not batch_file.is_file():
+            print(f'The provided path "{batch_file}" is not a file.')
+            sys.exit(1)
+
+        path_output = Path(self.options.output_dir)
+        if not path_output.exists():
+            path_output.mkdir(parents=True, exist_ok=True)
