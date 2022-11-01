1c5e8ecfb9531e97001ab4e7fbdf32841f126f27
Pascal Horton <pascal.horton@terranum.ch>
2022-11-01 20:59:03 +0100

    Adding tests for action instantiation.

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,4 +1,6 @@
+import importlib
 import os
+import tempfile
 import types
 
 import pytest
@@ -28,3 +30,17 @@ def test_controller_instance_fails_if_config_file_is_not_found():
 def test_controller_instance_succeeds():
     options = types.SimpleNamespace(config_file=DIR_PATH + '/files/config.yaml')
     asv.Controller(options)
+
+
+def test_controller_can_identify_non_existing_actions():
+    assert not hasattr(importlib.import_module('atmoswing_vigicrues'), 'FakeAction')
+
+
+def test_controller_can_instantiate_actions():
+    assert hasattr(importlib.import_module('atmoswing_vigicrues'), 'ExportBdApBp')
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        options = asv.Options(
+            types.SimpleNamespace(config_file=DIR_PATH + '/files/config.yaml',
+                                  bdapbp_output_dir=tmp_dir))
+        fct = getattr(importlib.import_module('atmoswing_vigicrues'), 'ExportBdApBp')
+        fct(options)
