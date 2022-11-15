dd4aaa86f7cee630f11cd4fbd243b9a49692b945
Pascal Horton <pascal.horton@terranum.ch>
2022-11-15 13:26:08 +0100

    New options management.

--- atmoswing_vigicrues/__main__.py
+++ atmoswing_vigicrues/__main__.py
@@ -8,14 +8,8 @@ def main() -> int:
         description="Traite les prévisions et les exportations d'AtmoSwing pour "
                     "le réseau Vigicrues.")
     parser.add_argument(
-        'batch_file', type=str,
-        help="chemin vers le fichier batch listant les méthodes d\'analogie.")
-    parser.add_argument(
         '--config-file', type=str, required=False,
         help="fichier de configuration du présent module.")
-    parser.add_argument(
-        '--output-dir', type=str, required=False, metavar='DIR',
-        help="répertoire de sortie pour enregistrer les résultats.")
 
     args = parser.parse_args()
 
--- atmoswing_vigicrues/options.py
+++ atmoswing_vigicrues/options.py
@@ -29,7 +29,6 @@ class Options:
         self.config = None
         self._check_options()
         self._load_config()
-        self._merge_config_options()
 
     @property
     def options(self):
@@ -60,7 +59,7 @@ class Options:
         -------
             Vrai (True) si l'option existe, faux (False) sinon.
         """
-        if hasattr(self.options, key) and getattr(self.options, key):
+        if key in self.config and self.config[key]:
             return True
         return False
 
@@ -78,14 +77,14 @@ class Options:
             La valeur de l'option.
         """
         if self.has(key):
-            return getattr(self.options, key)
+            return self.config[key]
         raise asv.OptionError(key)
 
     def _check_options(self):
         """ Contrôle que certaines options de base sont définies. """
         if self.options is None:
             raise asv.OptionError("Les options fournies sont vides.")
-        if self.options.config_file is None:
+        if not hasattr(self.options, 'config_file') or self.options.config_file is None:
             raise asv.OptionError(
                 "Le chemin du fichier de configuration n'a pas été fourni.")
 
@@ -95,26 +94,3 @@ class Options:
         with open(self.options.config_file) as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
 
-    def _merge_config_options(self):
-        """
-        Assemble les options définies en lignes de commandes et celles du
-        fichier de configuration. La ligne de commande prévaut.
-        """
-        self.options.output_dir = self._find_option('output_dir', mandatory=True)
-        self.options.batch_file = self._find_option('batch_file', mandatory=True)
-        self.options.pre_actions = self._find_option('pre_actions')
-        self.options.post_actions = self._find_option('post_actions')
-        self.options.bdapbp_output_dir = self._find_option('bdapbp_output_dir')
-        self.options.bdapbp_file_name = self._find_option('bdapbp_file_name')
-        self.options.scores_output_dir = self._find_option('scores_output_dir')
-        self.options.scores_file_name = self._find_option('scores_file_name')
-
-    def _find_option(self, key, mandatory=False):
-        """ Cherche une valeur dans les arguments passés ou le fichier config. """
-        if hasattr(self.options, key) and getattr(self.options, key):
-            return getattr(self.options, key)
-        if key in self.config and self.config[key]:
-            return self.config[key]
-        if mandatory:
-            raise asv.OptionError(key)
-        return None
