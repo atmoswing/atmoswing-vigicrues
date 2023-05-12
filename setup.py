53fc12ec3e58dcb6efa169b662dd5162ff5d2d46
Pascal Horton <pascal.horton@terranum.ch>
2023-05-12 17:02:34 +0200

    Bumping version number.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="atmoswing-vigicrues",
-        version="1.0.4",
+        version="1.1.0",
         author="Pascal Horton",
     )
